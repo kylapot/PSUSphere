43492aa4707bf7830b6655ab1dbda26c6f79597e
kylapot <kylapot>
2024-04-19 03:09:08 +0800

    new req

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0zoteoh_utrvt6nk3&almdw&y940y1x2#0^d$4xm673=hqp4hw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
