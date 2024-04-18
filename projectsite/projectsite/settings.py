f90854ff6091907f3fcf92cdad3c8745862d83cc
kylapot <kylapot>
2024-04-19 07:22:34 +0800

    tapos na

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -130,3 +130,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGOUT_REDIRECT_URL = 'login'
+LOGIN_REDIRECT_URL = 'home'
+LOGIN_URL = 'login'
+
--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 
 from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView
 from studentorg import views
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -28,4 +29,8 @@ urlpatterns = [
     path('organization_list/add', OrganizationCreateView.as_view(), name='organization-add'),
     path('organization_list/edit/<int:pk>', OrganizationUpdateView.as_view(), name='organization-update'),
     path('organization_list/delete/<int:pk>', OrganizationDeleteView.as_view(), name='organization-delete'),
+    re_path(r'^login/$', auth_views.LoginView.as_view(
+        template_name='login.html'), name='login'),
+    re_path(r'^logout/$', auth_views.LogoutView.as_view(), name='logout'),
+
 ]
