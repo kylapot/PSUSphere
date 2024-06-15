414c53d1a66e52f5a6c541b8581563cf24000466
kylapot <kylamae1952@gmail.com>
2024-06-15 15:54:28 +0800

    update

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-0zoteoh_utrvt6nk3&almdw&y940y1x2#0^d$4xm673=hqp4hw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['psuenv.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
